1cc2e40226790cc29d5684f3f1125921cd80d157
Alexander Reitzel <funtimecoding@gmail.com>
2019-08-23 22:02:20 +0200

    configure cmake to work on windows, add exit command

--- src/main.c
+++ src/main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 static char input[2048];
 
-int main(int argc, char** argv) {
-    puts("ctrl+c to exit");
+int main(int argc, char **argv) {
+    puts("ctrl+c to exit, or enter exit");
 
     while (1) {
         fputs(">", stdout);
         fgets(input, 2048, stdin);
         printf("%s", input);
+
+        if (strcmp(input, "exit") == 0) {
+            break;
+        }
     }
 
     return 0;
