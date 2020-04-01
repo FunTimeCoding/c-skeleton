3a33276e0ccf94a06d0b5b06c4503680b744d0e1
Alexander Reitzel <funtimecoding@gmail.com>
2020-04-02 00:32:54 +0200

    synchronize with shell-skeleton

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM debian
+MAINTAINER Alexander Reitzel
+ADD script/docker/provision.sh /root/provision.sh
+RUN chmod +x /root/provision.sh
+RUN /root/provision.sh
+ADD . /c-skeleton
+ENTRYPOINT ["/c-skeleton/bin/cs"]
--- script/build.sh
+++ script/build.sh
@@ -2,8 +2,18 @@
 
 rm -rf build
 script/check.sh --ci-mode
+script/measure.sh --ci-mode
 gcc -std=c99 -Wall src/main.c -o build/cs
 
 if [ "${1}" = --run ]; then
     build/cs
 fi
+
+SYSTEM=$(uname)
+
+if [ "${SYSTEM}" = Linux ]; then
+    script/debian/package.sh
+fi
+
+# TODO: Finish implementation.
+#script/docker/build.sh
